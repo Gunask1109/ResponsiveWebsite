bbd82d8a428f231f122c4f78d0d883f1d68870eb
Gunask1109 <guna.sekar7045@gmail.com>
2020-06-15 22:12:54 +0530

    Responsive Website using php

---
+++ about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="stytle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+<?php include 'menu.php';  ?>
+    
+    <div class="jumbotrom">
+    <h1>Gunasekar Sivasankar</h1>
+        <p> I am Gunask</p>
+    
+    </div>
+    
+    <section class="my-5">
+    <div class="py-5">
+    
+        
+        <h3 class="text-center">About Us</h3>
+    </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="image/b4.jpg" class="img-fluid aboutimg">
+        </div>
+        <div class="col-lg-6 col-md-6 col-12">
+            <h2 class="display-4"> I am Gunasekar</h2>
+            <p class="py-2"> Royal Enfield is an Indian motorcycle manufacturing brand with the tag of "the oldest global motorcycle brand in continuous production"[3] manufactured in factories in Chennai in India. Licensed from Royal Enfield by the indigenous Indian Madras Motors, it is now a subsidiary of Eicher Motors Limited, an Indian automaker.[4] The company makes the Royal Enfield Bullet, and other single-cylinder and twin-cylinder motorcycles.[5] First produced in 1901, Royal Enfield is the oldest motorcycle brand in the world still in production, with the Bullet model enjoying the longest motorcycle production run of all time.</p>
+            <a href="about.php" class="btn btn-success"> Check More</a>
+        </div>
+    </div>
+    </div>
+    </section>
+    
+     <footer>
+    
+    <p class="p-3 bg-dark text-white text-center"> @gunasekar7045@gmail.com  </p>
+    
+    </footer>
+    
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="stytle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+<?php include 'menu.php';  ?>
+    
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/b1.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/b2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/b3.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<section class="my-5">
+    <div class="py-5">
+    
+        
+        <h3 class="text-center">About Us</h3>
+    </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="image/b4.jpg" class="img-fluid aboutimg">
+        </div>
+        <div class="col-lg-6 col-md-6 col-12">
+            <h2 class="display-4"> I am Gunasekar</h2>
+            <p class="py-2"> Royal Enfield is an Indian motorcycle manufacturing brand with the tag of "the oldest global motorcycle brand in continuous production"[3] manufactured in factories in Chennai in India. Licensed from Royal Enfield by the indigenous Indian Madras Motors, it is now a subsidiary of Eicher Motors Limited, an Indian automaker.[4] The company makes the Royal Enfield Bullet, and other single-cylinder and twin-cylinder motorcycles.[5] First produced in 1901, Royal Enfield is the oldest motorcycle brand in the world still in production, with the Bullet model enjoying the longest motorcycle production run of all time.</p>
+            <a href="about.php" class="btn btn-success"> Check More</a>
+        </div>
+    </div>
+    </div>
+    </section>
+    
+    <section class="my-5">
+    <div class="py-5">
+    
+        
+        <h3 class="text-center">Our Services</h3>
+    </div>
+    <div class="continer-fluid">
+        <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+            <div class="card">
+         <img class="card-img-top" src="image/b5.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Bike One</h4>
+    <p class="card-text">Some example text.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+            
+            </div>
+             <div class="col-lg-4 col-md-4 col-12">
+            <div class="card">
+         <img class="card-img-top" src="image/b8.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Bike One</h4>
+    <p class="card-text">Some example text.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+            
+            </div>
+             <div class="col-lg-4 col-md-4 col-12">
+            <div class="card">
+         <img class="card-img-top" src="image/b5.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Bike One</h4>
+    <p class="card-text">Some example text.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+            
+            </div>
+        </div>
+        </div>    
+        
+    </section>
+    
+<section class="my-5">
+    <div class="py-5">
+    <h3 class="text-center">Our Gallary</h3>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b5.jpg" class="img-fluid pb-4">
+          
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b5.jpg" class="img-fluid pb-4">
+            
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b8.jpg" class="img-fluid pb-4">
+            
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b5.jpg" class="img-fluid pb-4">
+            
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b8.jpg" class="img-fluid pb-4">
+            
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b8.jpg" class="img-fluid pb-4">
+            
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b5.jpg" class="img-fluid pb-4">
+            
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b8.jpg" class="img-fluid pb-4">
+            
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/b8.jpg" class="img-fluid pb-4">
+            
+            </div>
+            
+            </div>
+        
+        </div>
+    </section>
+    
+        
+<section class="my-5">
+    <div class="py-5">
+    
+        
+        <h3 class="text-center"> Gallary</h3>
+    </div>
+    <div class="w-50 m-auto">
+    
+    <form action="userinfo.php" method="post">
+        <div class="form-group">
+        <label>Username</label>
+            <input type="text" name="user" autocomplete="off" class="form-control">
+     </div>
+         <div class="form-group">
+        <label>Email Id</label>
+            <input type="text" name="email" autocomplete="off" class="form-control">
+     </div>
+        
+         <div class="form-group">
+        <label>Mobile No</label>
+            <input type="text" name="mobile" autocomplete="off" class="form-control">
+     </div>
+         <div class="form-group">
+        <label>Comments</label>
+            <textarea class="form-control" name="comment">
+             
+             </textarea>
+     </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        
+        
+        
+        
+        
+        </form>
+    
+    </div>
+    
+    
+    </section>
+   c
+    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
